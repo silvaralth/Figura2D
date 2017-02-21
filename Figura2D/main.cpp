ed8902866ff02316b80b4df50276340c72a14059
silvaralth <silvaralth@gmail.com>
2017-02-21 04:40:34 -0500

    Initial Commit

---
+++ Figura2D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Figura2D
+//
+//  Created by Cocoa on 21/02/17.
+//  Copyright © 2017 Sergio Ballen COD: 1629770 - 2711. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
